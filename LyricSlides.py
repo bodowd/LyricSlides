618c73b97eca8eb45f50af5c0c7b46ba09098a09
Bing <odowd.bing@gmail.com>
2021-06-06 22:52:06 +0200

    working on Google Slides API for one song

---
+++ LyricSlides.py
@@ -0,0 +1,282 @@
+"""
+Following: https://github.com/googleworkspace/python-samples/blob/master/slides/snippets/slides_snippets.py
+"""
+from typing import List
+
+
+def create_presentation(title: str, slides_service):
+    service = slides_service
+    body = {
+        'title': title
+    }
+    presentation = service.presentations().create(body=body).execute()
+    presentation_id = presentation.get('presentationId')
+    print(f'Created presentation with ID: {presentation_id}')
+    return presentation_id
+
+
+class Slides(object):
+    def __init__(self, presentation_id: str, slides_service: str, page_id: str):
+        self.presentation_id = presentation_id
+        self.slides_service = slides_service
+        self.page_id = page_id
+        self.left_box_id = 'Left_box_'+self.page_id
+        self.right_box_id = 'Right_box_'+self.page_id
+        self.song_numbers_box_id = 'Song_number_box_'+self.page_id
+        self.FONT_SIZE = 24
+
+    def create_slide(self, insertion_index: str):
+        service = self.slides_service
+        requests = [
+            {
+                'createSlide': {
+                    'objectId': self.page_id,
+                    'insertionIndex': insertion_index
+                }
+            }
+        ]
+        body = {
+            'requests': requests
+        }
+        response = service.presentations().batchUpdate(presentationId=self.presentation_id, body=body).execute()
+        create_slide_response = response.get('replies')[0].get('createSlide')
+        obj_id = create_slide_response.get('objectId')
+        print(f'Created slide with ID: {obj_id}')
+        return response
+
+    def create_textbox_with_text(self, lyrics_list: List,
+                                 song_numbers_str: str = 'DE123, E123, F123'):
+        service = self.slides_service
+
+        songNumbersBoxHeight = {
+            'magnitude': 50,
+            'unit': 'PT'
+        }
+        songNumbersBoxWidth = {
+            'magnitude': 680,
+            'unit': 'PT'
+        }
+
+        lyricsBoxSize = {
+            'magnitude': 300,
+            'unit': 'PT'
+        }
+
+        requests = [
+            # create the right text box
+            {
+                'createShape': {
+                    'objectId': self.right_box_id,
+                    'shapeType': 'TEXT_BOX',
+                    'elementProperties': {
+                        'pageObjectId': self.page_id,
+                        'size': {
+                            'height': lyricsBoxSize,
+                            'width': lyricsBoxSize
+                        },
+                        'transform': {
+                            'scaleX': 1,
+                            'scaleY': 1,
+                            'translateX': 380,
+                            'translateY': 50,
+                            'unit': 'PT'
+                        }
+                    }
+                }
+            },
+            # create left text box
+            {
+                'createShape': {
+                    'objectId': self.left_box_id,
+                    'shapeType': 'TEXT_BOX',
+                    'elementProperties': {
+                        'pageObjectId': self.page_id,
+                        'size': {
+                            'height': lyricsBoxSize,
+                            'width': lyricsBoxSize
+                        },
+                        'transform': {
+                            'scaleX': 1,
+                            'scaleY': 1,
+                            'translateX': 20,
+                            'translateY': 50,
+                            'unit': 'PT'
+                        }
+                    }
+                }
+            },
+            # create the title text box with song numbers
+            {
+                'createShape': {
+                    'objectId': self.song_numbers_box_id,
+                    'shapeType': 'TEXT_BOX',
+                    'elementProperties': {
+                        'pageObjectId': self.page_id,
+                        'size': {
+                            'height': songNumbersBoxHeight,
+                            'width': songNumbersBoxWidth
+                        },
+                        'transform': {
+                            'scaleX': 1,
+                            'scaleY': 1,
+                            'translateX': 20,
+                            'translateY': 5,
+                            'unit': 'PT'
+                        }
+                    }
+                }
+            },
+            # insert text into the box
+            {
+                'insertText': {
+                    'objectId': self.song_numbers_box_id,
+                    'insertionIndex': 0,
+                    'text': song_numbers_str
+                }
+            },
+            {
+                'insertText': {
+                    'objectId': self.left_box_id,
+                    'insertionIndex': 0,
+                    'text': lyrics_list,
+                }
+            },
+            {
+                'insertText': {
+                    'objectId': self.right_box_id,
+                    'insertionIndex': 0,
+                    'text': 'New Box Text Inserted'
+                }
+            }
+        ]
+        body = {
+            'requests': requests
+        }
+        response = service.presentations().batchUpdate(presentationId=self.presentation_id, body=body).execute()
+        create_shape_response = response.get('replies')[0].get('createShape')
+        obj_id = create_shape_response.get('objectId')
+        print(f'Created textbox with ID: {obj_id}')
+
+    def alter_text_format(self):
+        """
+        Alters the text format (fonts, centering...) in the text boxes, id for the boxes are already in the class from
+        instantiation
+        """
+        service = self.slides_service
+        requests = [
+            {
+                'updateParagraphStyle': {
+                    'objectId': self.song_numbers_box_id,
+                    'style': {
+                        'alignment': 'CENTER'
+                    },
+                    'fields': 'alignment'
+                }
+            },
+            {
+                'updateTextStyle': {
+                    'objectId': self.song_numbers_box_id,
+                    'style': {
+                        'bold': True,
+                        'fontFamily': 'Arial',
+                        'fontSize': {
+                            'magnitude': self.FONT_SIZE + 6,  # numbers slightly larger than lyrics
+                            'unit': 'PT'
+                        },
+                        'foregroundColor': {
+                            'opaqueColor': {
+                                'rgbColor': {
+                                    'blue': 1.0,
+                                    'green': 1.0,
+                                    'red': 1.0
+                                }
+                            }
+                        }
+                    },
+                    'fields': 'bold,foregroundColor,fontFamily,fontSize'
+                }
+            },
+            {
+                'updateTextStyle': {
+                    'objectId': self.left_box_id,
+                    'style': {
+                        'bold': True,
+                        'fontFamily': 'Arial',
+                        'fontSize': {
+                            'magnitude': self.FONT_SIZE,
+                            'unit': 'PT'
+                        },
+                        'foregroundColor': {
+                            'opaqueColor': {
+                                'rgbColor': {
+                                    'blue': 1.0,
+                                    'green': 1.0,
+                                    'red': 1.0
+                                }
+                            }
+                        }
+                    },
+                    'fields': 'bold,foregroundColor,fontFamily,fontSize'
+                }
+            },
+            {
+                'updateTextStyle': {
+                    'objectId': self.right_box_id,
+                    'style': {
+                        'bold': True,
+                        'fontFamily': 'Arial',
+                        'fontSize': {
+                            'magnitude': self.FONT_SIZE,
+                            'unit': 'PT'
+                        },
+                        'foregroundColor': {
+                            'opaqueColor': {
+                                'rgbColor': {
+                                    'blue': 1.0,
+                                    'green': 1.0,
+                                    'red': 1.0
+                                }
+                            }
+                        }
+                    },
+                    'fields': 'bold,foregroundColor,fontFamily,fontSize'
+                }
+            }
+        ]
+        body = {
+            'requests': requests
+        }
+        response = service.presentations().batchUpdate(presentationId=self.presentation_id, body=body).execute()
+        print(f'Updated the text style for shape with ID: {self.left_box_id}')
+        return response
+
+    def update_slide_background(self):
+        service = self.slides_service
+        requests = [
+            {
+                'updatePageProperties': {
+                    'objectId': self.page_id,
+                    'pageProperties': {
+                        'pageBackgroundFill': {
+                            'solidFill': {
+                                'color': {
+                                    'rgbColor': {
+                                        'red': 0.0,
+                                        'green': 0.0,
+                                        'blue': 1.0
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    'fields': 'pageBackgroundFill'
+                }
+            }
+        ]
+        body = {
+            'requests': requests
+        }
+        response = service.presentations().batchUpdate(presentationId=self.presentation_id,
+                                                       body=body).execute()
+        print(f'Updated the background for shape with ID: {self.left_box_id}')
+        return response
---
+++ main.py
@@ -0,0 +1,80 @@
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+import LyricSlides as ls
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/presentations',
+         'https://www.googleapis.com/auth/drive']
+
+
+#### Authentication code from google
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+service = build('slides', 'v1', credentials=creds)
+
+# create presentation
+presentation = ls.create_presentation(title='Test API', slides_service=service)
+
+# get lyrics
+verse_dict = {'1': ['Wie allumfassend, Herr, Du bist!',
+  'Von Gott sind wir in Dir –',
+  'Was Du für uns geworden bist,',
+  'So sehr genießen wir.'],
+ '2': ['Die Weisheit Gottes bist Du uns,',
+  'Gott rettet uns durch Dich;',
+  'Der Weg zur Rettung Du nur bist,',
+  'Allein und ewiglich.'],
+ '3': ['Du bist für uns Gerechtigkeit,',
+  'Der Gottes Recht erfüllt;',
+  'Wir alle sind gerecht in Dir,',
+  'Ins beste Kleid umhüllt.'],
+ '4': ['Herr, Du bist unsre Heiligkeit,',
+  'Umwandlung brauchen wir;',
+  'In Dir Du heiligst tadellos,',
+  'Uns gleichgestaltest Dir.'],
+ '5': ['Erlösung bist Du auch für uns,',
+  'Dein Abbild unser wird;',
+  'Verklären wirst Du unsern Leib,',
+  'Von Freiheit sublimiert.'],
+ '6': ['Wenn wir nun sinnen über Dich,',
+  'Genießen Dich, den Herrn,',
+  'So nähert sich Dein Kommen mehr,',
+  'Du bleibst nicht lange fern.'],
+ '7': ['Wie süß der Vorgeschmack doch ist,',
+  'So herrlich und so reich!',
+  'Wir sehnen uns bei Dir zu sein,',
+  'In Füll erfahrn Dich.']}
+
+for k in verse_dict.keys():
+    verse = f'{k}\n'
+    for line in verse_dict[k]:
+        verse += (line+'\n')
+
+    # create slides
+    page_id = f'Slide_{k}'
+    slide = ls.Slides(presentation_id=presentation, slides_service=service, page_id=page_id)
+    response = slide.create_slide(insertion_index=str(k))
+    response = slide.create_textbox_with_text(lyrics_list=verse, song_numbers_str='DE123, E123, F123')
+    response = slide.alter_text_format()
+    response = slide.update_slide_background()
+
