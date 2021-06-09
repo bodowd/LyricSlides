b9bf4c011a9f9226ab1e5d9909960ecc3286847b
Bing <odowd.bing@gmail.com>
2021-06-09 22:04:15 +0200

    added comments

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 class Config:
     presentation_title = 'Test API'
+    # German hymn numbers
     de_hymn_numbers = [93,123,10]
+    # English hymn numbers
     e_hymn_numbers = [1,2,3]
     f_hymn_numbers = [4,5,6]
